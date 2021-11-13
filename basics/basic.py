0b451ee1560b696d998f96c3044b9272af6ab833
atmosinf <73055038+atmosinf@users.noreply.github.com>
2021-11-13 14:28:15 +0200

    added screenshots

--- basics/basic.py
+++ basics/basic.py
@@ -8,7 +8,12 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('gray',gray)
 
 # blur an image
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('blur', blur)
 
+# edge cascade
+canny = cv.Canny(img, 125, 175)
+cv.imshow('canny',canny)
 
 
 cv.waitKey(0)
